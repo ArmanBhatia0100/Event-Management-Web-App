569358e1ccc84c1578acec8d61ed5ddc449803c0
Arman Bhatia <arman.bhatia.1407@gmail.com>
2024-10-16 22:17:13 -0400

    minimal ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
+import Announcements from "./pages/Announcements";
 import Events from "./pages/Events";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-;
-
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Events></Events>
+      {/* <Events></Events> */}
+      <Announcements />
     </>
   );
 }
