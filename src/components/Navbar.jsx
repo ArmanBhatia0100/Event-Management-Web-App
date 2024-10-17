0251b5f0b1cde770eb0b63bc666b2c3746a35670
Arman Bhatia <arman.bhatia.1407@gmail.com>
2024-10-16 21:06:00 -0400

    added icons to the navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
+import HomeIcon from "@mui/icons-material/Home";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 const Navbar = () => {
   return (
     <div className="flex flex-col bg-gray-300 items-center p-3 justify-around text-sm">
-      <div className="">item1</div>
-      <div className="">item2</div>
-      <div className="">item3</div>
-      <div className="">item4</div>
+      <div>
+        <button type="button">
+          <HomeIcon sx={{ fontSize: "2rem" }} />
+        </button>
+      </div>
+      <div>
+        <button type="button">
+          <CalendarMonthIcon sx={{ fontSize: "2rem" }} />
+        </button>
+      </div>
+      <div>
+        <button type="button">
+          <CampaignIcon sx={{ fontSize: "2rem" }} />
+        </button>
+      </div>
+      <div>
+        <button type="button">
+          <AccountCircleIcon sx={{ fontSize: "2rem" }} />
+        </button>
+      </div>
     </div>
   );
 };
